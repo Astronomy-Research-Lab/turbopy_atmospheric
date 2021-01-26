f9007c124236287e005b43a14d64b14650f1b7d9
Aaron Ostenfeld <aaronemma@icloud.com>
2021-01-26 15:00:20 -0500

    Create methods

--- core_code/astro_physics.py
+++ core_code/astro_physics.py
@@ -1,1 +1,16 @@
+import numpy as np
+from turbopy import Simulation, PhysicsModule
 
+class Projectile(PhysicsModule):
+    def __init__(self, owner: Simulation, input_data: dict):
+        super().__init__(owner, input_data)
+        pass
+    
+    def initialize(self):
+        pass
+    
+    def exchange_resources(self):
+        pass
+    
+    def update(self):
+        pass
