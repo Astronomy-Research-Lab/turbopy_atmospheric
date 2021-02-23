be809c9c7426f2e4de023ce6640663351ba33d7d
Aaron Ostenfeld <aaronemma@icloud.com>
2021-02-23 15:49:00 -0500

    Fill clock, modules, tools

--- core_code/simulate.py
+++ core_code/simulate.py
@@ -2,10 +2,15 @@ import numpy as np
 import turbopy
 
 configuration = {
-        "Grid": {},
-        "Clock": {},
-        "PhysicsModules": {},
-        "Tools": {},
+        "Clock": {"start_time": 0,
+                  "end_time": 10,
+                  "dt": 0.1},
+        "PhysicsModules": {"mass": 1,
+                           "c_d": 0.5,
+                           "area": 1,
+                           "x0": [0, 3881000],
+                           "pusher": "Leapfrog"},
+        "Tools": {"Leapfrog": {}},
         "Diagnostics": {}
         }
 
