f0c21868c9185bc1836912df29ddf0260cf53a67
Owen Grannis <owengrannis@gmail.com>
2021-02-23 15:15:17 -0500

    Move atmosphere data from atmos_module to astro_physics

--- core_code/astro_physics.py
+++ core_code/astro_physics.py
@@ -11,10 +11,21 @@ class Projectile(PhysicsModule):
         self.p_h = None
         self.area = input_data.get('area', 1)
         self.push = owner.find_tool_by_name(input_data["pusher"]).push
+
+        self.top = input_data.get("top")
+        self.step = input_data.get("alt_step")
+        self.altitude = np.zeros(int(self.maximum/self.step))
+        self.density = self.altitude[:]
+        self.temperature = self.altitude[:]
+        self.pressure = self.altitude[:]
     
     def initialize(self):
         self.position[:] = np.array(self._input_data["x0"])
         self.velocity[:] = np.array(self._input_data["v0"])
+
+        self.altitude[:] = list(range(int(self.maximum/self.step)))
+        self.altitude[:] = [alt*self.step for alt in self.altitude]
+        self.density[:], self.temperature[:], self.pressure[:] = coesa76(self.altitude)
     
     def exchange_resources(self):
         self.publish_resource({"Projectile:Position": self.position})
@@ -23,5 +34,5 @@ class Projectile(PhysicsModule):
         self.publish_resource({"Projectile:Mass": self.mass})
     
     def update(self):
-        "update p_h here"
+        self.p_h = self.density.get(int((self.position[0, 1]/self.step)))
         self.push(self.position, self.velocity, self.mass, self.c_d, self.p_h, self.area)
