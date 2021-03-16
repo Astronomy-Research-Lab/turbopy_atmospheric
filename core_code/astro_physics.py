406d7c92f6b30fd2394973a1652cebab5f265c5a
Aaron Ostenfeld <aaronemma@icloud.com>
2021-03-16 15:44:56 -0400

    Start fixing math

--- core_code/astro_physics.py
+++ core_code/astro_physics.py
@@ -13,37 +13,35 @@ class Projectile(PhysicsModule):
 
         self.mass = input_data.get('mass', 1)
         self.c_d = input_data.get('c_d', 1)
+        self.p_h = self._input_data["p_h"]
         self.mach = None
-        self.p_h = None
         self.area = input_data.get('area', 1)
         self.push = owner.find_tool_by_name(input_data["pusher"]).push
 
-        self.top = input_data.get("top")
-        self.step = input_data.get("alt_step")
-        self.altitude = np.zeros(int(self.maximum/self.step))
-        self.density = self.altitude[:]
-        self.temperature = self.altitude[:]
-        self.pressure = self.altitude[:]
+        self.top = input_data.get("top", 100000)
+        self.step = input_data.get("alt_step", 1000)
+        self.altitude = np.zeros(int(self.top/self.step))
+        self.density = coesa76(self.altitude)[0]
+        self.temperature = coesa76(self.altitude)[1]
+        self.pressure = coesa76(self.altitude)[2]
+        print(self.position[0, 1])
     
     def initialize(self):
         self.position[:] = np.array(self._input_data["x0"])
         self.velocity[:] = np.array(self._input_data["v0"])
 
-        self.altitude[:] = list(range(int(self.maximum/self.step)))
+        self.altitude[:] = list(range(int(self.top/self.step)))
         self.altitude[:] = [alt*self.step for alt in self.altitude]
         self.density[:], self.temperature[:], self.pressure[:] = coesa76(self.altitude)
-        
-        self.c_d = self._input_data["c_d"]
-        self.p_h = self._input_data["p_h"]
+        self.mach = self.sound_constant * math.sqrt(self.temperature[int((self.position[0, 1] - 6378000)//self.step)])
     
     def exchange_resources(self):
         self.publish_resource({"Projectile:Position": self.position})
         self.publish_resource({"Projectile:Velocity": self.velocity})
-        self.publish_resource({"Projectile:F_drag": self.f_drag})
-        self.publish_resource({"Projectile:Mass": self.mass}
+        self.publish_resource({"Projectile:Mass": self.mass})
 
     def update(self):
         self.p_h = self.density.get(int((self.position[0, 1]/self.step)))
-        self.mach = self.sound_constant * math.sqrt(self.temperature.get(int((self.position[0, 1]/self.step))))
+        self.mach = self.sound_constant * math.sqrt(self.temperature[self.position[0, 1]//self.step])
         self.mach =  math.sqrt((self.velocity[0 , 0] ** 2) + (self.velocity[0, 1] ** 2))/self.mach
         self.push(self.position, self.velocity, self.mass, self.c_d, self.p_h, self.area)
