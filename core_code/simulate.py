18c913c98724794a1e5b57c2ad54e7aaea94800d
Aaron Ostenfeld <aaronemma@icloud.com>
2021-04-15 15:47:45 -0400

    Begin gui

--- core_code/simulate.py
+++ core_code/simulate.py
@@ -3,6 +3,29 @@ from turbopy import Simulation, ComputeTool, PhysicsModule, Diagnostic
 from astro_tools import Leapfrog
 from astro_physics import Projectile
 from astro_diagnostics import ProjectileDiagnostic
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Initial conditions")
+frm = tk.Frame(master = window)
+
+lbl_vel = tk.Label(master = frm, text = "Entry velocity (km/s):")
+ent_vel = tk.Entry(master = frm, width = 20)
+lbl_ang = tk.Label(master = frm, text = "Entry angle (\N{DEGREE SIGN}):")
+ent_ang = tk.Entry(master = frm, width = 20)
+lbl_coef = tk.Label(master = frm, text = "Drag coefficient:")
+ent_coef = tk.Entry(master = frm, width = 20)
+
+frm.pack()
+lbl_vel.grid(row = 0, column = 0, padx = 5, sticky = 'w')
+ent_vel.grid(row = 0, column = 1, padx = 5)
+lbl_ang.grid(row = 1, column = 0, padx = 5, sticky = 'w')
+ent_ang.grid(row = 1, column = 1, padx = 5)
+lbl_coef.grid(row = 2, column = 0, padx = 5, sticky = 'w')
+ent_coef.grid(row = 2, column = 1, padx = 5)
+
+window.mainloop()
+
 
 configuration = {
     "Clock": {"start_time": 0,
